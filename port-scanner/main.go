a64a6806f411e8aa3f94e787a428cdb4bf0deecc
twelcon <peeyush.tyagi@getastra.com>
2023-06-28 19:05:42 +0530

    Base structure for port-scanner
    Signed-off-by: twelcon <peeyush.tyagi@getastra.com>

--- port-scanner/main.go
+++ port-scanner/main.go
@@ -3,10 +3,18 @@ package portscanner
 import (
 	"encoding/json"
 	"endgame/utils"
+	"net/url"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
+// This function can be used for starting a port scanner on the context.target endpoint
 func StartScan(scanData utils.ScanData) error {
 
 	newLog := log.WithFields(log.Fields{
@@ -14,6 +22,107 @@ func StartScan(scanData utils.ScanData) error {
 	})
 	newLog.Info("Starting new port scanner")
 
+	parsedURL, err := url.Parse(scanData.Context.Target)
+	if err != nil {
+		panic(err)
+	}
+	newLog.Infof("Domain name found -> %s", parsedURL.Host)
+
+	cmd := exec.Command("rustscan", "-a", parsedURL.Host, "-r", "1-65535", "-u", "5000", "-g")
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		newLog.Panicf("Error occurred while rust scan -> %s", err.Error())
+		return err
+	}
+
+	newLog.Infof("Response found -> %s", stdout)
+
+	re := regexp.MustCompile(`\[([\d,]+)\]`)
+	portFoundString := re.FindStringSubmatch(string(stdout))
+
+	portFoundArray := strings.Split(portFoundString[1], ",")
+
+	if len(portFoundArray) < 1 {
+		newLog.Info("No port found for scanning")
+	}
+
+	newLog.Info("RustScan might have overloaded the server, let it rest.")
+	time.Sleep(10 * time.Second)
+
+	err = serviceDetection(&portFoundArray, newLog)
+	if err != nil {
+		newLog.Errorf("Error occurred while service detection -> %s", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// This function will be used for doing service detection on a specific port
+func serviceDetection(portFoundArray *[]string, newLog *log.Entry) error {
+	newLog.Info("Starting service detection")
+
+	file, _ := os.Open("resource/portscanner_service_probes.json")
+	decoder := json.NewDecoder(file)
+
+	configuration := []PortScannerProbe{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		newLog.Panicf("Error occurred while reading config -> %s", err.Error())
+		newLog.Panicf("Please create `config.json` file in proper format")
+		log.Exit(1)
+	}
+	file.Close()
+
+	newLog.Infof("Scanner Probe loaded -> %d", len(configuration))
+
+	for _, port := range *portFoundArray {
+		newLog.Infof("Checking for port -> %s", port)
+		err := checkForPort(port, newLog)
+		if err != nil {
+			newLog.Errorf("Error occurred while further examining port -> %s", err.Error())
+		}
+	}
+
+	return nil
+}
+
+func checkForPort(port string, newLog *log.Entry) error {
+	for _, excludedPort := range ExcludedList {
+		if port == excludedPort {
+			newLog.Infof("%s is in excluded list, continuing without service detection.", port)
+			return nil
+		}
+	}
+
+	newLog.Info("Running NULL probe test...")
+	return nil
+
+}
+
+func bannerMatcher(host string, addr string, port int, probes []PortScannerProbe, phase string, newLog *log.Entry) error {
+	var rarityInt int
+	var err error
+	for _, probe := range probes {
+		newLog.Infof("Current probe : %s", probe.Probe.ProbeName)
+		rarityInt, err = strconv.Atoi(probe.Rarity.Rarity)
+		if err != nil {
+			newLog.Errorf("Error occurred converting rarity to int -> %s", err.Error())
+			continue
+		}
+
+		if probe.Rarity.Rarity != "" && rarityInt > 5 && phase == "Excluded" {
+			return nil
+		}
+
+	}
+
+	return nil
+}
+
+// This function can be used for raising alerts in port scanning
+func raiseAlert(scanData utils.ScanData, newLog *log.Entry) error {
 	newLog.Info("Raising alert for high severity port")
 	newAlertBody := AlertBody{
 		Name:        "Target Has Ports Open for Critical Services",
@@ -40,5 +149,6 @@ func StartScan(scanData utils.ScanData) error {
 	}
 
 	utils.SendRequestToWebhook(&scanData, newLog, "alert", resp)
+
 	return nil
 }
