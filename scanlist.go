d3b455c9ab7379c4b017046956e8cc0cc922a330
karthik.uj <karthik.uj@getastra.com>
2023-07-11 23:01:20 +0530

    minor changes
    Signed-off-by: karthik.uj <karthik.uj@getastra.com>

--- scanlist.go
+++ scanlist.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	portscanner "endgame/port-scanner"
 	"endgame/utils"
 
-	// portscanner "endgame/port-scanner"
+	portscanner "endgame/port-scanner"
 	subdomaintakeover "endgame/subdomain-takeover"
 )
 
